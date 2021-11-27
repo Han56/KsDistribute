34ad425f48570581d83690bae385440589bb22fa
Han56 <2279627931@qq.com>
2021-11-27 15:44:00 +0800

    创建Flink程序测试模块

--- studyFlink/src/main/java/testFiles/ReadAlignFile.java
+++ studyFlink/src/main/java/testFiles/ReadAlignFile.java
@@ -1,6 +1,18 @@
-package testFiles;/**
-*@description 功能描述
-*@author han56
-*@create 2021/11/25 下午3:22
-*/public class ReadAlignFile {
+package testFiles;
+
+import org.junit.Test;
+
+/**
+ * @author han56
+ * @description 功能描述
+ * 按行读取对齐映射文件
+ * @create 2021/11/25 下午3:22
+ */
+public class ReadAlignFile {
+
+    @Test
+    public void readAlign(){
+
+    }
+
 }
